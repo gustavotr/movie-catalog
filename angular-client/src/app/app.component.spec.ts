6e133ef98b73cd5487ecadaa75c5109110fcb44d
Gustavo T Rudiger <gustavo.rudiger@ufpr.br>
2019-07-25 18:37:22 -0300

    Started TDD

--- angular-client/src/app/app.component.spec.ts
+++ angular-client/src/app/app.component.spec.ts
@@ -16,16 +16,28 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-client'`, () => {
+  it(`should have as title 'Movie Catalog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-client');
+    expect(app.title).toEqual('Movie Catalog');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have an 'addMovie' function`, () => {
+
+  });
+
+  it(`should render an 'Add Movie' button`, () =>{
+    const fixture = TestBed.createComponent(AppComponent);    
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-client!');
+    expect(compiled.querySelector('#add-movie-action').textContent).toContain('Add Movie');
   });
+
+  it(`should have and a list of movies`, () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.movieList).toBeDefined();
+  });
+
+
 });
