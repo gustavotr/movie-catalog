fe59545025059933fedcd2e95cdbba5ab4aa212e
Gustavo T Rudiger <gustavo.rudiger@ufpr.br>
2019-07-26 11:33:21 -0300

    Tested movie seeds

--- express-server/server.js
+++ express-server/server.js
@@ -2,13 +2,24 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+const { populate } = require('./model/Movie');
+const mongoose = require('mongoose');
+
+// Connect to the database
+const dbURL = "mongodb://database/movie-catalog";
+mongoose.connect(dbURL, { useNewUrlParser: true })
+    .then(() => console.info(`Successfully connected to MongoDB database on "${dbURL}"`))
+    .catch((error) => console.error(`Cannot connect to MongoDB: ${error}`));
+
+// Populate the database if empty
+populate();
 
 // Get our API routes
 const api = require('./routes/api');
 
 const app = express();
 
-// Parsers for POST data
+// Parsers for POST data 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
